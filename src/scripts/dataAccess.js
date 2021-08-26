194d366304c27e4b9f20c4e36d014c6042e364e3
Alicia AyersLott <amayers92@gmail.com>
2021-08-26 13:17:12 -0500

    adding some notes

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,23 +1,25 @@
-const applicationState = {
-        requests: []
-}
+//Here we are making an object to hold application state. fetch the API requests state and store that external API state in application state
 
-const API = "http://localhost:8088"
+const applicationState = {
+        requests: [] //12. store the data referenced by serviceRequests here
+} 
+
+const API = "http://localhost:8088" //7. see that API is referencing a URL here
 const mainContainer = document.querySelector("#container")
 
-export const fetchRequests = () => {
-    return fetch(`${API}/requests`)
-        .then(response => response.json())
+export const fetchRequests = () => { //5. define function fetchRequests AKA get JSONdata from API and turn into JS
+    return fetch(`${API}/requests`) //6. take ${API}/requests and pass it to fetch. what does API reference??
+                                    //8. we are fetching the requests data section of the API's webstite
+        .then(response => response.json()) //9. take response of promise{} made in step 6 and translate to JS language
         .then(
-            (serviceRequests) => {
-                // Store the external state in application state
-                applicationState.requests = serviceRequests
+            (serviceRequests) => { //10. take data that is now in JS and pass into this .then(); data is now called 'serviceRequests'
+                applicationState.requests = serviceRequests   // 11. Store the external state in application state
             }
         )
 }
-export const getRequests = () => {
-    return applicationState.requests.map(request => ({
-        ...request}
+export const getRequests = () => { //13. define getRequests(). the purpose is to return service requests data we stored in step 12.
+    return applicationState.requests.map(request => ({ //HERE: map COPIES the requests into one new array
+        ...request} //HERE: spread operator breaks the array apart into 1 object per request
     ))
 }
 
